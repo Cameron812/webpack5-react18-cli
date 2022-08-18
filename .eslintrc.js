9e2eb08752a7d3a9afce1ff3d6567f8aadab56e4
CameronFu <531019326@qq.com>
2022-08-18 14:39:38 +0800

    resolve eslint prettier conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-  extends: ['react-app'],
+  extends: ['react-app', 'prettier'],
   parserOptions: {
     babelOptions: {
       presets: [
         ['babel-preset-react-app', false],
-        'babel-preset-react-app/prod',
-      ],
-    },
+        'babel-preset-react-app/prod'
+      ]
+    }
   },
+  plugins: ['prettier'],
   rules: {
     'no-console': 2,
-  },
+    'prettier/prettier': 'error'
+  }
 };
